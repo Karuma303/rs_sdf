c1b0144149617d828d66a48874c61067cecbac1b
Karuma <info@karuma.de>
2020-07-10 13:23:56 +0200

    more cleanup in image/example/test folders

--- src/input.rs
+++ src/input.rs
@@ -71,6 +71,8 @@ pub fn get_source_from_png_file_input(file_path: &str) -> Result<SourceField, Fi
 mod tests {
     use crate::input::{get_source_from_png_file_input, FileInputError};
 
+    const TEST_ASSET_BASE_PATH: &str = r"assets\test\";
+
     #[test]
     fn input_does_not_exist() {
         let res = get_source_from_png_file_input("some_random_path_that_does_not_exist.dat");
@@ -79,19 +81,19 @@ mod tests {
 
     #[test]
     fn input_is_not_png() {
-        let res = get_source_from_png_file_input(r"assets\some_asset_that_is_not_png.dat");
+        let res = get_source_from_png_file_input(r"assets\test\invalid_file.dat");
         assert_eq!(res.err().unwrap(), FileInputError::InvalidFileType);
     }
 
     #[test]
     fn input_file_is_no_valid_rgba() {
-        let res = get_source_from_png_file_input(r"assets\SDF_Test_Texture_2.png");
+        let res = get_source_from_png_file_input(r"assets\test\test_rgb_1x1_black.png");
         assert_eq!(res.err().unwrap(), FileInputError::InvalidImageFormat);
     }
 
     #[test]
     fn input_file_is_valid() {
-        let res = get_source_from_png_file_input(r"assets\SDF_Test_Texture_RGBA.png");
+        let res = get_source_from_png_file_input(r"assets\test\test_rgba_1x1_fully_transparent.png");
         assert!(res.is_ok());
     }
 
@@ -99,15 +101,15 @@ mod tests {
     fn generated_source_field_contains_valid_data() {
 
         // check fully transparent 1x1 image
-        let s1 = get_source_from_png_file_input(r"assets\rgba_1x1_fully_transparent.png").unwrap();
+        let s1 = get_source_from_png_file_input(r"assets\test\test_rgba_1x1_fully_transparent.png").unwrap();
         assert_eq!(s1.data[0], false);
 
         // check fully opaque 1x1 image
-        let s2 = get_source_from_png_file_input(r"assets\rgba_1x1_90_percent_opaque.png").unwrap();
+        let s2 = get_source_from_png_file_input(r"assets\test\test_rgba_1x1_90_percent_opaque.png").unwrap();
         assert_eq!(s2.data[0], true);
 
         // check checkered 2x2 image
-        let s3 = get_source_from_png_file_input(r"assets\rgba_2x2_checkerboard.png").unwrap();
+        let s3 = get_source_from_png_file_input(r"assets\test\test_rgba_2x2_checkerboard.png").unwrap();
         assert_eq!(s3.data[0], true); // tl
         assert_eq!(s3.data[1], false); // tr
         assert_eq!(s3.data[2], false); // bl
