70f4b5d4bb84632c1b771239548b06eb9c9d8232
Karuma <info@karuma.de>
2020-07-20 11:47:23 +0200

    updated examples

--- examples/images.rs
+++ examples/images.rs
@@ -25,18 +25,21 @@ fn main() {
     generate_sdf("example_1_rgba_512x512.png",
                  "example_1_512x512.png",
                  ExportType::UnsignedInnerDistance,
+                 ImageOutputChannelDepth::Eight,
                  ImageOutputChannels::One);
 
     // 1.2. Export PNG with 8-bit outer distance
     generate_sdf("example_1_rgba_512x512.png",
                  "example_1_512x512.png",
                  ExportType::UnsignedOuterDistance,
+                 ImageOutputChannelDepth::Eight,
                  ImageOutputChannels::One);
 
     // 1.3. Export PNG with 8-bit inner and outer distance (distances added in one channel)
     generate_sdf("example_1_rgba_512x512.png",
                  "example_1_512x512.png",
                  ExportType::UnsignedInnerOuterDistance,
+                 ImageOutputChannelDepth::Eight,
                  ImageOutputChannels::One);
 
     // 2. Demonstrate single- and dual-channel export
@@ -45,12 +48,14 @@ fn main() {
     generate_sdf("example_2_rgba_512x512.png",
                  "example_2_512x512.png",
                  ExportType::UnsignedInnerOuterDistance,
+                 ImageOutputChannelDepth::Eight,
                  ImageOutputChannels::One);
 
     // 2.2. Export PNG with inner and outer distance separated to two 8-bit channels
     generate_sdf("example_2_rgba_512x512.png",
                  "example_2_512x512_2_channel.png",
                  ExportType::UnsignedInnerOuterDistance,
+                 ImageOutputChannelDepth::Eight,
                  ImageOutputChannels::Two);
 
 
@@ -58,6 +63,7 @@ fn main() {
     generate_sdf("example_8_rgba_512x512.png",
                  "example_8_512x512.png",
                  ExportType::UnsignedInnerOuterDistance,
+                 ImageOutputChannelDepth::Eight,
                  ImageOutputChannels::One);
 
     /*
@@ -73,26 +79,32 @@ fn main() {
 }
 
 
-fn generate_sdf(source_image_name: &str, target_image_name: &str, export_type: ExportType, num_channels: ImageOutputChannels) {
+fn generate_sdf(source_image_name: &str,
+                target_image_name: &str,
+                export_type: ExportType,
+                bit_depth: ImageOutputChannelDepth,
+                num_channels: ImageOutputChannels) {
     let mut image_path_buff = PathBuf::new();
     image_path_buff.push(BASE_ASSET_FOLDER);
     image_path_buff.push(source_image_name);
 
     let source_image_path = image_path_buff.into_os_string().into_string().unwrap();
 
-    let prefix: String = match export_type {
-        ExportType::UnsignedInnerDistance => String::from("idf"),
-        ExportType::UnsignedOuterDistance => String::from("odf"),
-        ExportType::UnsignedInnerOuterDistance => String::from("cdf"),
-    };
+    let mut prefixes: Vec<String> = Vec::new();
+    prefixes.push(get_type_prefix(&export_type));
+    prefixes.push(get_num_channels_prefix(&num_channels));
+    prefixes.push(get_bit_depth_prefix(&bit_depth));
 
-    let target_image_path = get_output_image_file_path(target_image_name, &prefix);
+    let prefix: String = prefixes.join("_");
+
+    let target_image_path = get_output_image_file_path(target_image_name,
+                                                       prefix.as_str());
 
     let g = DistanceGenerator::new()
         .input(PngInput::new(&source_image_path))
         .output(PngOutput::new(&target_image_path,
                                num_channels,
-                               ImageOutputChannelDepth::Eight))
+                               bit_depth))
         .export_type(export_type)
         .processor(EightSideSweepProcessor {});
 
@@ -107,13 +119,34 @@ fn get_output_image_file_path(filename: &str, prefix: &str) -> String {
     file_path_buff.into_os_string().into_string().unwrap()
 }
 
+fn get_type_prefix(export_type: &ExportType) -> String {
+    match export_type {
+        ExportType::UnsignedInnerDistance => String::from("idf"),
+        ExportType::UnsignedOuterDistance => String::from("odf"),
+        ExportType::UnsignedInnerOuterDistance => String::from("cdf"),
+    }
+}
+
+fn get_bit_depth_prefix(channel_depth: &ImageOutputChannelDepth) -> String {
+    match channel_depth {
+        ImageOutputChannelDepth::Eight => String::from("8bit"),
+        ImageOutputChannelDepth::Sixteen => String::from("16bit"),
+        ImageOutputChannelDepth::ThirtyTwo => String::from("32bit"),
+    }
+}
+
+fn get_num_channels_prefix(num_channels: &ImageOutputChannels) -> String {
+    match num_channels {
+        ImageOutputChannels::One => String::from("1chan"),
+        ImageOutputChannels::Two => String::from("2chan"),
+    }
+}
+
 fn display_result(result: &Result<(), String>, source_image_path: &String, target_image_path: &String) {
     match result {
-        Ok(_) => {
-            println!("generated sdf {} for input image {}", target_image_path, source_image_path);
-        }
-        Err(err) => {
-            println!("error generating sdf: {}", err);
-        }
+        Ok(_) =>
+            println!("generated sdf {} for input image {}", target_image_path, source_image_path),
+        Err(err) =>
+            println!("error generating sdf: {}", err),
     }
 }
