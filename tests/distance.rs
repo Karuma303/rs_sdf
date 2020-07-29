a39779e237f36fb83c5bbe632d5e753670182e51
Karuma <info@karuma.de>
2020-07-29 13:15:05 +0200

    adds test module for distances

---
+++ tests/distance.rs
@@ -0,0 +1,83 @@
+#[cfg(test)]
+mod tests {
+
+    #[test]
+    fn euclidean_8_bit() {
+        panic!()
+    }
+
+    #[test]
+    fn euclidean_16_bit() {
+        panic!()
+    }
+
+    #[test]
+    fn euclidean_squared_8_bit() {
+        panic!()
+    }
+
+    #[test]
+    fn euclidean_squared_16_bit() {
+        panic!()
+    }
+
+    #[test]
+    fn chebyshev_8_bit() {
+        panic!()
+    }
+
+    #[test]
+    fn chebyshev_16_bit() {
+        panic!()
+    }
+
+    #[test]
+    fn rectilinear_8_bit() {
+        panic!()
+    }
+
+    #[test]
+    fn rectilinear_16_bit() {
+        panic!()
+    }
+
+    #[test]
+    fn cartesian_8_bit() {
+        panic!()
+    }
+
+    #[test]
+    fn cartesian_16_bit() {
+        panic!()
+    }
+
+    #[test]
+    fn nearest_cell_index_8_bit() {
+        panic!()
+    }
+
+    #[test]
+    fn nearest_cell_index_16_bit() {
+        panic!()
+    }
+
+    #[test]
+    fn nearest_cell_index_offset_8_bit() {
+        panic!()
+    }
+
+    #[test]
+    fn nearest_cell_index_offset_16_bit() {
+        panic!()
+    }
+
+    #[test]
+    fn nearest_cell_position_8_bit() {
+        panic!()
+    }
+
+    #[test]
+    fn nearest_cell_position_16_bit() {
+        panic!()
+    }
+}
