bc81dbc8ca97e45cd9a8c8627d89b1976ab31bf9
Karuma <info@karuma.de>
2020-08-04 16:37:09 +0200

    replaced vector of bool with bitvec

--- src/data/source.rs
+++ src/data/source.rs
@@ -1,11 +1,20 @@
+use bitvec::vec::{BitVec};
+
+/// Spatial input type as a source for distance field generation.
+/// An input field is defined by its dimensions and a binary data block.
+/// The number of items inside the data block match the dimension of the input
+/// and a set data value specifies a foreground cell, whereas an unset data
+/// value  specifies a background cell.
 #[derive(Debug)]
-pub struct SourceField {
-    pub data: Vec<bool>,
+pub struct InputField {
+    pub data: BitVec,
     pub width: u32,
     pub height: u32,
 }
 
-impl SourceField {
+/// The implementation of an InputField provides several constructors
+/// for building a field from arrays of primitive types.
+impl InputField {
 
     // TODO
     // The SourceField should be kinda independent from the input buffer.
@@ -14,9 +23,12 @@ impl SourceField {
 
     pub fn from_bytes(buffer: &[u8], threshold: u8, width: u32, height: u32) -> Self {
         Self::error_if_invalid_params(width, height, buffer.len());
-        let mut data: Vec<bool> = vec!();
+        // let mut data: Vec<bool> = vec!();
+        let mut data = BitVec::new();
+        // buffer.iter().for_each(|elem| data.push(*elem > threshold));
         buffer.iter().for_each(|elem| data.push(*elem > threshold));
-        SourceField {
+        InputField {
+            // data,
             data,
             width,
             height,
@@ -25,9 +37,9 @@ impl SourceField {
 
     pub fn from_booleans(buffer: &[bool], width: u32, height: u32) -> Self {
         Self::error_if_invalid_params(width, height, buffer.len());
-        let mut data: Vec<bool> = vec!();
+        let mut data = BitVec::new();
         buffer.iter().for_each(|elem| data.push(*elem));
-        SourceField {
+        InputField {
             data,
             width,
             height,
@@ -47,31 +59,7 @@ impl SourceField {
         }
     }
 
-    /*
-    pub fn new(buffer: &[u8], width: u32, height: u32) -> Self {
-        if width == 0 {
-            panic!("width must be greater than zero"); // maybe an error type "incorrect dimensions" would be better here!
-        }
-        if height == 0 {
-            panic!("height must be greater than zero"); // maybe an error type "incorrect dimensions" would be better here!
-        }
-
-        let size = (width * height) as usize;
-        if buffer.len() != size {
-            panic!("buffer size should be {}", width * height);
-        }
-        let mut data: Vec<bool> = vec!();
-        buffer.iter().for_each(|elem| data.push(*elem > 127));
-        SourceField {
-            data,
-            width,
-            height,
-        }
-    }
-
-     */
-
     pub fn invert(&mut self) {
-        self.data.iter_mut().for_each(|i| *i = !*i);
+        self.data.iter_mut().for_each(|mut i| *i = !*i);
     }
 }
