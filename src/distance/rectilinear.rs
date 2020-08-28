b1207b224490d48ca4520e2450eddbfe33e35b75
Karuma <info@karuma.de>
2020-08-28 14:47:43 +0200

    implemented rectilinear distance (u32)

--- src/distance/rectilinear.rs
+++ src/distance/rectilinear.rs
@@ -1,12 +1,19 @@
+use crate::distance::OneDimensionalDistanceCalculation;
+use crate::data::Cell;
+
 /// The Rectilinear distance (also know as the Manhattan distance) to the nearest cell.
 /// It is defined as the sum of the horizontal distance and the vertical distance.
 /// The distance is a single, unsigned value.
 pub struct RectilinearDistance;
 
-/*
-impl Calculator<u16> for RectilinearDistance {
-	fn calculate(cell: &Cell) -> u16 {
-		todo!()
+impl OneDimensionalDistanceCalculation<u32> for RectilinearDistance {
+	fn calculate(cell: &Cell) -> u32 {
+		if let Some((nearest_x, nearest_y)) = cell.nearest_cell_position {
+			let dx: u32 = (nearest_x as i32 - cell.x as i32).abs() as u32;
+			let dy: u32 = (nearest_y as i32 - cell.y as i32).abs() as u32;
+			dx + dy
+		} else {
+			0u32
+		}
 	}
 }
-*/
--- tests/distance.rs
+++ tests/distance.rs
@@ -1,81 +1,126 @@
 #[cfg(test)]
 mod tests {
-    use rs_sdf::data::{Cell, CellLayer};
+	use rs_sdf::data::{Cell, CellLayer};
+	use rs_sdf::distance::DistanceType;
+	use rs_sdf::distance::euclid::{EuclideanDistance, EuclideanDistanceSquared};
+	use rs_sdf::distance::OneDimensionalDistanceCalculation;
+	use rs_sdf::distance::TwoDimensionalDistanceCalculation;
+	use rs_sdf::distance::cartesian::CartesianDistance;
+	use rs_sdf::distance::rectilinear::RectilinearDistance;
 
-    fn setup_cell(source_x: u16, source_y: u16, nearest_x: u16, nearest_y: u16) -> Cell {
-        Cell {
-            nearest_cell_position: Some((nearest_x, nearest_y)),
-            x: source_x,
-            y: source_y,
-            layer: CellLayer::Foreground,
-        }
-    }
+	fn setup_cell(source_x: u16, source_y: u16, nearest_x: u16, nearest_y: u16) -> Cell {
+		Cell {
+			nearest_cell_position: Some((nearest_x, nearest_y)),
+			x: source_x,
+			y: source_y,
+			layer: CellLayer::Foreground,
+		}
+	}
 
-    /*
-    #[test]
-    fn calculate_euclidean_distance() {
-        let fun : CalculationFunction<u16> = DistanceType::EuclideanDistance.calculator();
+	#[test]
+	fn calculate_euclidean_distance() {
+		// EuclideanDistance
 
-        // zero distance
-        let c = setup_cell(0, 0, 0, 0);
-        assert_eq!(fun(&c), 0u16);
+		let fun = EuclideanDistance::calculate;
 
-        let c = setup_cell(0, 0, 3, 4);
-        assert_eq!(fun(&c), 5u16);
+		// zero distance
+		let c = setup_cell(0, 0, 0, 0);
+		let res: u16 = fun(&c);
+		assert_eq!(res, 0u16);
 
-        let c = setup_cell(3, 4, 0, 0);
-        assert_eq!(fun(&c), 5u16);
+		let c = setup_cell(0, 0, 3, 4);
+		let res: u16 = fun(&c);
+		assert_eq!(res, 5u16);
 
-        // TODO: add some test for maximum range here!
-    }
-     */
+		let c = setup_cell(3, 4, 0, 0);
+		let res: u16 = fun(&c);
+		assert_eq!(res, 5u16);
 
-    /*
-    #[test]
-    fn calculate_euclidean_distance_squared() {
-        let fun : CalculationFunction<u16> = DistanceType::EuclideanDistanceSquared.calculator();
+		// TODO: add some test for maximum range here!
+	}
 
-        // zero distance
-        let c = setup_cell(0, 0, 0, 0);
-        assert_eq!(fun(&c), 0u16);
+	#[test]
+	fn calculate_euclidean_distance_squared() {
+		// EuclideanDistanceSquared
 
-        let c = setup_cell(0, 0, 3, 4);
-        assert_eq!(fun(&c), 25u16);
+		let f = EuclideanDistanceSquared::calculate;
 
-        let c = setup_cell(3, 4, 0, 0);
-        assert_eq!(fun(&c), 25u16);
+		// zero distance
+		let c = setup_cell(0, 0, 0, 0);
+		let res: u16 = f(&c);
+		assert_eq!(res, 0);
 
-        // TODO: add some test for maximum range here!
-    }
-    */
+		let c = setup_cell(0, 0, 3, 4);
+		let res: u16 = f(&c);
+		assert_eq!(res, 25);
 
-    #[test]
-    fn calculate_chebyshev_distance() {
-        unimplemented!()
-    }
+		let c = setup_cell(3, 4, 0, 0);
+		let res: u16 = f(&c);
+		assert_eq!(res, 25);
 
-    #[test]
-    fn calculate_rectilinear_distance() {
-        panic!()
-    }
+		// TODO: add some test for maximum range here!
+	}
 
-    #[test]
-    fn calculate_cartesian_distance() {
-        panic!()
-    }
+	#[test]
+	fn calculate_chebyshev_distance() {
+		unimplemented!()
+	}
 
-    #[test]
-    fn get_nearest_cell_index() {
-        panic!()
-    }
+	#[test]
+	fn calculate_rectilinear_distance() {
+		let f = RectilinearDistance::calculate;
 
-    #[test]
-    fn get_nearest_cell_index_offset() {
-        panic!()
-    }
+		// zero distance
+		let c = setup_cell(0, 0, 0, 0);
+		let res: u32 = f(&c);
+		assert_eq!(res, 0);
 
-    #[test]
-    fn get_nearest_cell_position() {
-        panic!()
-    }
+		let c = setup_cell(0, 0, 3, 4);
+		let res: u32 = f(&c);
+		assert_eq!(res, 7);
+
+		let c = setup_cell(3, 4, 0, 0);
+		let res: u32 = f(&c);
+		assert_eq!(res, 7);
+
+		// TODO: add some test for maximum range here!
+	}
+
+	#[test]
+	fn calculate_cartesian_distance() {
+
+		// Cartesian Distance
+
+		let f = CartesianDistance::calculate;
+
+		// zero distance
+		let c = setup_cell(0, 0, 0, 0);
+		let res: (i32, i32) = f(&c);
+		assert_eq!(res, (0, 0));
+
+		let c = setup_cell(0, 0, 3, 4);
+		let res: (i32, i32) = f(&c);
+		assert_eq!(res, (3, 4));
+
+		let c = setup_cell(3, 4, 0, 0);
+		let res: (i32, i32) = f(&c);
+		assert_eq!(res, (-3, -4));
+
+		// TODO: add some test for maximum range here!
+	}
+
+	#[test]
+	fn get_nearest_cell_index() {
+		panic!()
+	}
+
+	#[test]
+	fn get_nearest_cell_index_offset() {
+		panic!()
+	}
+
+	#[test]
+	fn get_nearest_cell_position() {
+		panic!()
+	}
 }
